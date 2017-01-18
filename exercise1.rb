fa38bcaf428066962e57a40b6a14c85fcb67db3e
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-18 12:44:04 -0500

    Exercise 1 finished with deposit, withdraw and gain interest

--- exercise1.rb
+++ exercise1.rb
@@ -1,7 +1,21 @@
 class BankAccount
+
+  attr_reader :balance, :interest_rate
   def initialize(balance, interest_rate)
     @balance  = balance
     @interest_rate = interest_rate
-    
+
+  end
+
+  def deposit=(amount)
+    @balance += amount.to_i
+  end
+
+  def withdraw=(amount)
+    @balance -= amount
+  end
+
+  def gain_interest
+    @balance += (@balance * @interest_rate)
   end
 end
